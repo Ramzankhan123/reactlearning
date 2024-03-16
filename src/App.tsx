55c61e6ff0be301971da9439c5be161da29cd744
ramzan2020 <ramzan.2020@tecorb.co>
2024-03-16 15:38:50 +0530

    routeing

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './component/Header'
+import Products from './component/product'
+import Router from './routing'
+
+function App() {
+  return (
+   <div>
+     <Header />
+     <Router />
+     {/* <Products /> */}
+   </div>
+  )
+}
+
+export default App
