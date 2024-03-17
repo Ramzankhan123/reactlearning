b81331c69eeca98432ddcb96ed57cce3f91d7497
ramzan2020 <ramzan.2020@tecorb.co>
2024-03-17 15:24:31 +0530

    json server added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import {useState} from "react";
+import { useState } from "react";
 import Header from "./component/header";
 import Footer from "./component/footer";
 import { Route, Routes } from "react-router-dom";
 import HomepageScreen from "./screens/homepage";
 import LoginpageScreen from "./screens/loginpage";
+import ErrorpageScreen from "./screens/errorpage";
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -19,10 +20,14 @@ function App() {
         title="Awesome Music Store"
         date={new Date().toISOString()}
       />
-      <Routes>
-        <Route path="/" element={<HomepageScreen/>} />
-        <Route path="/login" element={<LoginpageScreen/>} />
-      </Routes>
+      <main className="container mx-auto p-8">
+        <Routes>
+          <Route path="/" element={<HomepageScreen />} />
+          <Route path="/login" element={<LoginpageScreen />} />
+          <Route path="*" element={<ErrorpageScreen />} />{" "}
+          {/* when there is not route found screen navigate to here */}
+        </Routes>
+      </main>
       {/* <LoginForm /> */}
       <Footer />
     </>
