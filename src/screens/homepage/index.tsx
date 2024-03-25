2c12732d3233fda6e1c80ea93a42fa20d01c3e79
ramzan2020 <ramzan.2020@tecorb.co>
2024-03-25 17:12:48 +0530

    added bootstrap

--- src/screens/homepage/index.tsx
+++ src/screens/homepage/index.tsx
@@ -2,32 +2,70 @@ import { useEffect, useState } from "react";
 import AlbumList from "../../component/album-list";
 import { getAlbums } from "../../model/albums/api";
 import { Album } from "../../interfaces/Album";
+import { Carousel } from "react-bootstrap";
 
 const HomepageScreen = () => {
-    const [topAlbums, setTopAlbums] = useState<null | Album[]>(null);
-    const [latestAlbums, setLatestAlbums] = useState<null | Album[]>(null);
-  
-    useEffect(() => {
-      const callApi = async () => {
-        const topAlbumsData = await getAlbums("top-albums");
-        const latestAlbumsData = await getAlbums("latest-albums");
-        setTopAlbums(topAlbumsData);
-        setLatestAlbums(latestAlbumsData);
-      };
-      callApi();
-    }, []);
+  const [topAlbums, setTopAlbums] = useState<null | Album[]>(null);
+  const [latestAlbums, setLatestAlbums] = useState<null | Album[]>(null);
+
+  useEffect(() => {
+    const callApi = async () => {
+      const topAlbumsData = await getAlbums("top-albums");
+      const latestAlbumsData = await getAlbums("latest-albums");
+      setTopAlbums(topAlbumsData);
+      setLatestAlbums(latestAlbumsData);
+    };
+    callApi();
+  }, []);
 
   return (
     <>
       <section className="mb-12">
-        <img
+        <Carousel>
+          <Carousel.Item>
+            <img
+              src="https://via.placeholder.com/1200x400"
+              alt="Banner"
+              className="rounded w-full"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="https://via.placeholder.com/1200x400"
+              alt="Banner"
+              className="rounded w-full"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="https://via.placeholder.com/1200x400"
+              alt="Banner"
+              className="rounded w-full"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        {/* <img
           src="https://via.placeholder.com/1200x400"
           alt="Banner"
           className="rounded w-full"
-        />
+        /> */}
       </section>
-      <AlbumList list={topAlbums} title="Top Albums"/>
-      <AlbumList list={latestAlbums} title="Latest Albums"/>
+      <AlbumList list={topAlbums} title="Top Albums" />
+      <AlbumList list={latestAlbums} title="Latest Albums" />
     </>
   );
 };
